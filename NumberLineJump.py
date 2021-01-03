78a907361601dc65aa4af2d360a3d6effd87f932
UDAY BAG <uday625@Nemesis-2.local>
2021-01-03 17:23:50 -0500

    added number line jump

--- NumberLineJump.py
+++ NumberLineJump.py
@@ -1,8 +1,23 @@
-def NumberLineJump(n):
-    pass
-
+def NumberLineJump(x1, v1, x2, v2):
+        while True:
+            if x1 > x2:
+                return 'NO'
+            elif v2 >= v1:
+                return 'NO'
+            x1 = x1 +v1
+            x2 = x2 +v2
+            if x1==x2:
+                return 'YES'
 
 
 if __name__ == '__main__':
-    n=21
-    NumberLineJump(n)
+    param =[0, 3, 4, 2]  #YES
+    param =[0, 2, 5, 3]  #NO
+    param =[21, 6, 47, 3] #NO
+    param =[43, 2, 70, 2] #NO
+
+    x1 =param[0]
+    v1 =param[1]
+    x2 =param[2]
+    v2 =param[3]
+    NumberLineJump(x1, v1, x2, v2)
